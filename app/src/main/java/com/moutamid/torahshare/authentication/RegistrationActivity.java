b3752ccb17f3a30f8e10015948a0dbd031035793
Moutamid <lumianokia188@gmail.com>
2022-07-30 03:53:55 +0500

    initial commit

--- app/src/main/java/com/moutamid/torahshare/authentication/RegistrationActivity.java
+++ app/src/main/java/com/moutamid/torahshare/authentication/RegistrationActivity.java
@@ -159,7 +159,8 @@ public class RegistrationActivity extends AppCompatActivity {
 
         // Configure Google Sign In
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestIdToken("890062472575-05ue90j9694eg6eorj1lhjoknkj3oqja.apps.googleusercontent.com")
+//                .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
 
